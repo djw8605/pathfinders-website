3a8364bba93e8c1211a42dc09fa82ae599ca40de
Cannon Lock <clock@wisc.edu>
2024-12-13 12:06:23 -0600

    Mobile Design Polish

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 import style from "./Header.module.css";
-import Link from "next/link";
 
 const Header = () => {
   return <header className={style.header}>
     <div className={style.nameContainer}>
       <span className={style.name}>Pathfinders</span>
-    </div>
-    <div className={style.navSection}>
-      <Link className={style.link} href="#about">About</Link>
-      <Link className={style.link} href="#strategies">Strategies</Link>
-      <Link className={style.link} href="#team">Team</Link>
     </div>
   </header>;
 };
